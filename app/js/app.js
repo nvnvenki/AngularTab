98acb1e286654128d72d8cad84490dabd21a319a
venki <nvnvenki@gmail.com>
2015-10-23 20:31:51 +0530

    Added edit and delete shortcut functionality

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,7 @@
 var newTab = angular.module('newTab', ['ui.bootstrap']);
+newTab.config(['$modalProvider', function($modalProvider)  {
+  $modalProvider.options.animation = true;
+}]);
 // Initialize tooltip
  $('[data-toggle="tooltip"]').tooltip()
 // Initialize bootstrap material
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,4 @@
-newTab.controller('newTabCtrl', function($scope, $timeout, $uibModal) {
-
+newTab.controller('newTabCtrl', function($scope, $timeout, $uibModal, $window) {
 	// Build the date object
 	$scope.date = {};
 
@@ -34,22 +33,75 @@ newTab.controller('newTabCtrl', function($scope, $timeout, $uibModal) {
 
 	$scope.addShortCut = function() {
 		var modalInstance = $uibModal.open({
-	        animation: true,
-	        templateUrl: 'templates/add-shortcut.html',
-	        controller: 'addShortCutModalCtrl'
+	        templateUrl: 'templates/add-edit-shortcut.html',
+	        controller: 'shortCutModalCtrl',
+	         resolve: {
+	        	newTabLink: function () {
+	        		return undefined;
+	        	}
+	       }
 	    });
 	    modalInstance.result.then(function (newShortcut) {
 	    	$scope.newTablinks = JSON.parse(localStorage.getItem('newTablinks'));
 	    	$scope.newTablinks.push(newShortcut);
 	    	localStorage.setItem('newTablinks', JSON.stringify($scope.newTablinks));
 	    });
-	}
+	};
+
+	$scope.openLink = function(link) {
+		$window.open('http://' + link.url + "/");
+	};
+
+	$scope.removeShortcut = function(newTabLink, $event) {
+		$event.stopPropagation();
+		var modalInstance = $uibModal.open({
+	        templateUrl: 'templates/delete-shortcut.html',
+	        controller: 'deleteShortCutModalCtrl'
+	    });
+	    modalInstance.result.then(function (deleteShortcut) {
+	    	if(deleteShortcut) {
+	    		 $scope.newTablinks = JSON.parse(localStorage.getItem('newTablinks'));
+				var indexToDelete = $scope.newTablinks.indexOf(newTabLink);
+		  		$scope.newTablinks.splice(indexToDelete, 1);     
+				localStorage.setItem('newTablinks', JSON.stringify($scope.newTablinks));
+	    	}
+	    });
+		
+	};
+
+	$scope.editShortcut = function(newTabLink, $event) {
+		$event.stopPropagation();
+		var indexToEdit = $scope.newTablinks.indexOf(newTabLink);
+
+		var modalInstance = $uibModal.open({
+	        templateUrl: 'templates/add-edit-shortcut.html',
+	        controller: 'shortCutModalCtrl',
+		    resolve: {
+	        	newTabLink: function () {
+	        		return newTabLink;
+	        	}
+	       }
+	    });
+	    modalInstance.result.then(function (modifiedShortcut) {
+	    	$scope.newTablinks = JSON.parse(localStorage.getItem('newTablinks'));
+	    	$scope.newTablinks[indexToEdit] = modifiedShortcut;
+	    	localStorage.setItem('newTablinks', JSON.stringify($scope.newTablinks));
+	    });
+	};
 
 });
 
-newTab.controller('addShortCutModalCtrl', function($scope,  $modalInstance) {
-	$scope.name = "";
-	$scope.url = "";
+newTab.controller('shortCutModalCtrl', function($scope,  $modalInstance, newTabLink) {
+	$scope.buttonText = "Add";
+	if(newTabLink != undefined) {
+		$scope.name = newTabLink.name;
+		$scope.url = newTabLink.url;
+		$scope.buttonText = "Ok"
+	} else {	
+		$scope.name = "";
+		$scope.url = "";
+	}
+
 	$scope.ok = function () {
 		// No values entered
 		if($scope.name.trim() === "" || $scope.url.trim === "") {
@@ -63,6 +115,18 @@ newTab.controller('addShortCutModalCtrl', function($scope,  $modalInstance) {
 	};
 
 	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+	};
+});
+
+newTab.controller('deleteShortCutModalCtrl', function($scope,  $modalInstance) {
+	$scope.yes = function() {
+		$modalInstance.close({
+			deleteShortcut: true
+		});
+	};
+
+	$scope.no = function () {
 		$modalInstance.dismiss('cancel');
 	};
 });
